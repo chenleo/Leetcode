865ed2d18cd8b01bb5046d36e863577456506a5b
Leo Chen <maverickcc@gmail.com>
2013-10-02 20:18:10 +0200

    Create Same_Tree.cpp

---
+++ Cpp/Same_Tree.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if (p == NULL && q == NULL)
+            return true;
+        else if (p == NULL || q == NULL)
+            return false;
+        else
+            return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
