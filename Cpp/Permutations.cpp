62269f67174b131f7213f7515d70cf1a6ab6abdb
Leo Chen <maverickcc@gmail.com>
2013-12-17 15:24:52 +0100

    Create Permutations.cpp

---
+++ Cpp/Permutations.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int> > result;
+        sort(num.begin(), num.end());
+        do {
+            result.push_back(num);
+        } while (
+            next_permutation(num.begin(), num.end())
+        );
+        return result;
+    }
+};
